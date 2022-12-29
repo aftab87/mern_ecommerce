44fb7e7d54796bf4fe68a03c5e64f78ec2999723
aftab87 <safdar_aftab@hotmail.com>
2022-12-29 02:07:54 -0500

    added redux

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,30 +1,33 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
-import axios from 'axios';
+import { listProducts } from '../redux/actions/productActions';
 
 const HomePage = () => {
-    const [products, setProducts] = useState([]);
+    const dispatch = useDispatch();
+
+    const { loading, error, products } = useSelector(state => state.productList)
 
     useEffect(() => {
-        const fetchProducts = async () => {
-            const res = await axios.get('/api/products')
-
-            setProducts(res.data);
-        }
-        fetchProducts();
-    }, []);
+        dispatch(listProducts())
+    }, [dispatch]);
 
     return (
         <>
             <h1>Latest Products</h1>
-            <Row>
-                {products.map(product => (
-                    <Col key={product.name.trim().replace(" ", "_")} sm={12} md={6} lg={4} xl={3}>
-                        <Product product={product} />
-                    </Col>
-                ))}
-            </Row>
+            {loading
+                ? <h2>Loading ... </h2>
+                : error
+                    ? <h3>{error}</h3>
+                    : <Row>
+                        {products.map(product => (
+                            <Col key={product.name.trim().replace(" ", "_")} sm={12} md={6} lg={4} xl={3}>
+                                <Product product={product} />
+                            </Col>
+                        ))}
+                    </Row>
+            }
         </>
     )
 }
